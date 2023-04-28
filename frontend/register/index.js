fb83e2508f2f124cfc41d6663249a0223a886806
cmpseduardo <camposeduardov@gmail.com>
2023-04-28 11:27:48 -0300

    28-04-2023

--- frontend/register/index.js
+++ frontend/register/index.js
@@ -13,3 +13,23 @@ function backForma() {
     document.querySelector(".pessoa-juridica").classList.add("hidden")
     document.querySelector(".forma-cadastro").classList.remove("hidden")
 }
+
+function cadastrarPessoaFisica() {
+    const dados = {
+        "nome": document.querySelector("#nome-pessoa-fisica"),
+        "cpf": document.querySelector("#cpf"),
+        "email": document.querySelector("#email-pessoa-fisica"),
+        "telefone": `${document.querySelector(".ddd-pessoa-fisica")}` + `${document.querySelector("#numero-celular-pessoa-fisica")}`,
+        "senha": document.querySelector("#senha-2-pessoa-fisica"),
+    }
+}
+
+function cadastrarPessoaJuridica(){
+    const dados = {
+        "nome": document.querySelector("#nome-pessoa-fisica"),
+        "cpf": document.querySelector("#cpf"),
+        "email": document.querySelector("#email-pessoa-fisica"),
+        "telefone": `${document.querySelector(".ddd-pessoa-fisica")}` + `${document.querySelector("#numero-celular-pessoa-fisica")}`,
+        "senha": document.querySelector("#senha-2-pessoa-fisica"),
+    } 
+}
