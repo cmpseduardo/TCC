fef45216b41f9c4e0b777618d244971654c29125
cmpseduardo <camposeduardov@gmail.com>
2023-05-04 10:20:58 -0300

    04-05-2023

---
+++ frontend/campaign/index.js
@@ -0,0 +1,36 @@
+const idCampanha = (new URLSearchParams(window.location.search).get('id'))
+
+function carregar() {
+    console.log("aaaa")
+    fetch("http://localhost:3300/campanha")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data)
+            data.forEach(campanha => {
+                if (campanha.id == idCampanha) {
+                    let nomeCampanha = document.querySelector("h1")
+                    let nomeOrganizador = document.querySelector("h2")
+                    let img = document.querySelector(".img-campanha")
+                    let meta = document.querySelector(".valor-meta")
+                    let arrecadado = document.querySelector(".valor-arrecadado")
+
+                    let resumo = document.querySelector(".breve-resumo")
+                    let descricao = document.querySelector(".descricao")
+
+                    nomeCampanha.innerHTML = campanha.titulo
+                    nomeOrganizador.innerHTML = campanha.organizador.nome
+                    img.src = `../../back/${campanha.imagens[0].caminho_imagem}`
+                    meta.innerHTML = `R$${campanha.valor_meta}`
+                    arrecadado.innerHTML = `R$${campanha.valor_arrecadado}`
+
+                    resumo.innerHTML = campanha.objetivo
+                    descricao.innerHTML = campanha.descricao
+                }
+                // let novoItem = itemCard.cloneNode(true)
+
+                // novoItem.classList.remove("hidden")
+            })
+        })
+}
--- frontend/home/index.js
+++ frontend/home/index.js
@@ -11,14 +11,17 @@ function carregar() {
 
                 let novoItem = itemCard.cloneNode(true)
 
-                novoItem.classList.remove("modelo")
+                novoItem.classList.remove("hidden")
 
+                idCampanha = novoItem.querySelector("#id-campanha")
                 img = novoItem.querySelector(".img-campanha")
                 nomeCampanha = novoItem.querySelector(".nome-campanha")
                 nomeOrganizador = novoItem.querySelector(".nome-organizador")
                 descricao = novoItem.querySelector(".descricao-campanha")
 
-                img.src = `../../back/${campanha.imagens[1].caminho_imagem}`
+                idCampanha.setAttribute("id", campanha.id);
+                idCampanha.classList.add("hidden")
+                img.src = `../../back/${campanha.imagens[0].caminho_imagem}`
                 nomeCampanha.innerHTML = campanha.titulo
                 nomeOrganizador.innerHTML = campanha.organizador.nome
                 descricao.innerHTML = campanha.descricao
@@ -40,12 +43,12 @@ function carregarNovas() {
 
                 novoItem.classList.remove("modelo")
 
-                img = novoItem.querySelector(".img-campanha")
-                nomeCampanha = novoItem.querySelector(".nome-campanha")
-                nomeOrganizador = novoItem.querySelector(".nome-organizador")
-                descricao = novoItem.querySelector(".descricao-campanha")
+                let img = novoItem.querySelector(".img-campanha")
+                let nomeCampanha = novoItem.querySelector(".nome-campanha")
+                let nomeOrganizador = novoItem.querySelector(".nome-organizador")
+                let descricao = novoItem.querySelector(".descricao-campanha")
 
-                img.src = `../../back/${campanha.imagens[1].caminho_imagem}`
+                img.src = `../../back/${campanha.imagens[0].caminho_imagem}`
                 nomeCampanha.innerHTML = campanha.titulo
                 nomeOrganizador.innerHTML = campanha.organizador.nome
                 descricao.innerHTML = campanha.descricao
@@ -53,4 +56,9 @@ function carregarNovas() {
                 document.querySelector(".cards-new").appendChild(novoItem);
             })
         })
+}
+
+function abrirCampanha(e) {
+    idCampanha = e.querySelector(".id-campanha")
+    window.location.href = `http://127.0.0.1:5500/frontend/campaign/index.html?id=${idCampanha.id}`
 }
