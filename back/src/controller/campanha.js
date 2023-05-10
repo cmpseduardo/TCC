a210a082434dedd6ec0795442548d6af9c878ef0
cmpseduardo <camposeduardov@gmail.com>
2023-05-10 11:26:10 -0300

    10-05-2023

--- back/src/controller/campanha.js
+++ back/src/controller/campanha.js
@@ -5,92 +5,92 @@ const prisma = new PrismaClient();
 
 const createCampanha = async (req, res) => { //OK
   let campanhas = await prisma.campanha.create({
-      data: req.body
+    data: req.body
   });
 
   res.status(200).json(campanhas).end();
 }
 
 
- const createImagem = async (req, res) => {
-   uploadUser.single('image')(req, res, async (err) => {
-     if (err) {
-       return res.status(400).json({
-         erro: true,
-         mensagem: "Erro: " + err.message
-       });
-     }
-     if (!req.file) {
-       return res.status(400).json({
-         erro: true,
-         mensagem: "Erro: upload não realizado!"
-       });
-     }
-     // Criar o registro da imagem no banco de dados
-     const campanhaId = parseInt(req.body.campanhaId);
-     const caminhoImagem = req.file.path;
+const createImagem = async (req, res) => {
+  uploadUser.single('image')(req, res, async (err) => {
+    if (err) {
+      return res.status(400).json({
+        erro: true,
+        mensagem: "Erro: " + err.message
+      });
+    }
+    if (!req.file) {
+      return res.status(400).json({
+        erro: true,
+        mensagem: "Erro: upload não realizado!"
+      });
+    }
+    // Criar o registro da imagem no banco de dados
+    const campanhaId = parseInt(req.body.campanhaId);
+    const caminhoImagem = req.file.path;
 
-     // Verificar se a campanha com o ID fornecido existe
-     const campanha = await prisma.campanha.findUnique({
-       where: {
-         id: campanhaId,
-       },
-     });
-     if (!campanha) {
-       return res.status(400).json({
-         erro: true,
-         mensagem: "Erro: campanha não encontrada!"
-       });
-     }
-     const imagem = await prisma.imagemCampanha.create({
-       data: {
-         caminho_imagem: caminhoImagem,
-         campanha: {
-           connect: {
-             id: campanhaId,
-           },
-         },
-       },
-     });
-     return res.json({
-       erro: false,
-       mensagem: "Upload realizado com sucesso!",
-       imagem: imagem,
-     });
-   });
- };
+    // Verificar se a campanha com o ID fornecido existe
+    const campanha = await prisma.campanha.findUnique({
+      where: {
+        id: campanhaId,
+      },
+    });
+    if (!campanha) {
+      return res.status(400).json({
+        erro: true,
+        mensagem: "Erro: campanha não encontrada!"
+      });
+    }
+    const imagem = await prisma.imagemCampanha.create({
+      data: {
+        caminho_imagem: caminhoImagem,
+        campanha: {
+          connect: {
+            id: campanhaId,
+          },
+        },
+      },
+    });
+    return res.json({
+      erro: false,
+      mensagem: "Upload realizado com sucesso!",
+      imagem: imagem,
+    });
+  });
+};
 
 const readTudo = async (req, res) => {
   let campanhas = await prisma.campanha.findMany({
-      select: {
+    select: {
+      id: true,
+      tipo: true,
+      titulo: true,
+      descricao: true,
+      objetivo: true,
+      data_inicio: true,
+      prazo: true,
+      contato: true,
+      valor_meta: true,
+      valor_arrecadado: true,
+      atualizacoes: true,
+      chave_pix: true,
+      organizador: {
+        select: {
           id: true,
-          tipo: true,
-          titulo: true,
-          descricao: true,
-          objetivo: true,
-          data_inicio: true,
-          prazo: true,
-          contato: true,
-          valor_meta: true,
-          valor_arrecadado: true,
-          atualizacoes: true,
-          chave_pix: true,
-          organizador: {
-              select: {
-                  id: true,
-                  nome: true,
-                  email: true
-              }
-          },
-          imagens: {
-            select: {
-              id: true,
-              caminho_imagem: true,
-              }
-          }
+          nome: true,
+          email: true
+        }
+      },
+      imagens: {
+        select: {
+          id: true,
+          caminho_imagem: true,
+        }
       }
+    }
   });
-  
+
   res.status(200).json(campanhas).end();
 }
 
@@ -100,7 +100,7 @@ async function atualizarCampanha(req, res) {
 
   try {
     const campanha = await prisma.campanha.update({
-      where: { id: campanhaId },
+      where: { id: Number(campanhaId) },
       data: {
         descricao,
         prazo,
--- frontend/organizer/index.js
+++ frontend/organizer/index.js
@@ -35,9 +35,7 @@ function carregarMeuPerfil() {
 
 }
 
-function editarCampanha() {
-
-}
+var itemCard = document.querySelector(".card-completo")
 
 function carregarCampanhas() {
     document.querySelector(".campanha-icon").src = "../../docs/imgs/icon-doacao-white.png"
@@ -49,6 +47,78 @@ function carregarCampanhas() {
 
     document.querySelector(".option1").classList.add("selected-option")
     document.querySelector(".option2").classList.remove("selected-option")
+
+    fetch("http://localhost:3300/campanha")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            data.forEach(campanha => {
+
+                let novoItem = itemCard.cloneNode(true)
+
+                novoItem.classList.remove("ocultar")
+                document.querySelector(".descricao-textarea")
+
+                let id = novoItem.querySelector(".n-id")
+                let img = novoItem.querySelector(".img-organizador")
+                let nomeCampanha = novoItem.querySelector("#nome-campanha")
+                let meta = novoItem.querySelector("#meta")
+                let arrecadado = novoItem.querySelector("#arrecadado")
+                let descricao = novoItem.querySelector(".descricao-textarea")
+
+                id.innerHTML = campanha.id
+                img.src = `../../back/${campanha.imagens[0].caminho_imagem}`
+                nomeCampanha.value = campanha.titulo
+                meta.value = campanha.valor_meta
+                arrecadado.value = campanha.valor_arrecadado
+                descricao.innerHTML = campanha.descricao
+
+                document.querySelector(".main-campanhas").appendChild(novoItem);
+            })
+        })
+}
+
+function editarCampanha(e) {
+    e.querySelector("#arrecadado").disabled = false;
+    e.querySelector(".descricao-textarea").disabled = false;
+    e.querySelector("#salvar").classList.remove("ocultar");
+    if (!e.querySelector("#editar").classList.contains("ocultar")) {
+        e.querySelector("#editar").classList.add("ocultar");
+    }
+}
+
+
+function salvarCampanha(e) {
+    let id = e.querySelector(".n-id").innerHTML
+    let arrecadado = e.querySelector("#arrecadado").value
+    let descricao = e.querySelector(".descricao-textarea").value
+
+    const data = {
+        "valor_arrecadado": Number(arrecadado),
+        "descricao": descricao
+    }
+
+    fetch(`http://localhost:3300/campanha/${id}`, {
+        "method": 'PUT',
+        "headers": {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json())
+        .then(updatedUser => {
+            window.location.reload()
+            alert('Campanha atualizada com sucesso!', updatedUser)
+        }
+        )
+        .catch(error => console.error('Erro ao atualizar campanha.', error))
+
+    arrecadado.disabled = true;
+    descricao.disabled = true;
+
+    document.querySelector("#salvar").classList.add("ocultar");
+    document.querySelector("#editar").classList.remove("ocultar");
 }
 
 function editarPerfil() {
@@ -66,15 +136,63 @@ function editarPerfil() {
 }
 
 function salvarEdicao() {
-    document.querySelector("#input-nome-organizador").disabled = true
-    document.querySelector("#input-cpf-cnpj").disabled = true
-    document.querySelector("#input-email").disabled = true
-    document.querySelector("#input-celular").disabled = true
-    document.querySelector("#input-instagram").disabled = true
-    document.querySelector("#input-whatsapp").disabled = true
-    document.querySelector("#input-twitter").disabled = true
-    document.querySelector("#input-site").disabled = true
-    document.querySelector("#input-file").classList.add("ocultar")
-    document.querySelector("#editar-perfil").classList.remove("ocultar")
-    document.querySelector("#salvar-edicao").classList.add("ocultar")
+    let nomeOrganizador = document.querySelector("#input-nome-organizador").value
+    let cpfCnpj = document.querySelector("#input-cpf-cnpj").value
+    let email = document.querySelector("#input-email").value
+    let celular = document.querySelector("#input-celular").value
+    let instagram = document.querySelector("#input-instagram").value
+    let whatsapp = document.querySelector("#input-whatsapp").value
+    let twitter = document.querySelector("#input-twitter").value
+    let site = document.querySelector("#input-site").value
+    let file = document.querySelector("#input-file").value
+    let editar = document.querySelector("#editar-perfil")
+    let salvar = document.querySelector("#salvar-edicao")
+
+    nomeOrganizador.disabled = true
+    cpfCnpj.disabled = true
+    email.disabled = true
+    celular.disabled = true
+    instagram.disabled = true
+    whatsapp.disabled = true
+    twitter.disabled = true
+    site.disabled = true
+    file.classList.add("ocultar")
+    editar.classList.remove("ocultar")
+    salvar.classList.add("ocultar")
+
+    let body = {
+        // "id": 3,
+        "nome": nomeOrganizador,
+        // Se cpfCnpj = 11 {
+        "cpf": "581.579.687-85",
+        // }
+        // Se cpfCnpj = 14 {
+        "cnpj": "22706924000103",
+        // }
+        "email": "bora6822@uorak.com",
+        "telefone": "(14)1231-8117",
+        "senha": "$2b$10$fj7XyaXCM79oWIO9eat4OeNR4zPl7VS9zDsaMfX.4Kaf8radRa5GC",
+        "acessoid": 2
+    }
+
+    fetch(`http://localhost:3300/campanha/${id}`, {
+        "method": 'PUT',
+        "headers": {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json())
+        .then(updatedUser => {
+            window.location.reload()
+            alert('Campanha atualizada com sucesso!', updatedUser)
+        }
+        )
+        .catch(error => console.error('Erro ao atualizar campanha.', error))
+
+    arrecadado.disabled = true;
+    descricao.disabled = true;
+
+    document.querySelector("#salvar").classList.add("ocultar");
+    document.querySelector("#editar").classList.remove("ocultar");
 }
