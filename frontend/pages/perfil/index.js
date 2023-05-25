6ef1576155114715c121af80b6b8df95b97df8ff
cmpseduardo <camposeduardov@gmail.com>
2023-05-25 09:56:31 -0300

    25-05-2023

--- frontend/pages/perfil/index.js
+++ frontend/pages/perfil/index.js
@@ -21,3 +21,65 @@ function carregarPerfil() {
             descPerfil.innerHTML = item.telefone //Trocar para descrição
         })
 }
+
+function contarValorTotal() {
+    let valorTotal = 0;
+
+    fetch("http://localhost:3300/campanha")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            data.forEach(item => {
+                if (item.organizador.id == idPerfil) {
+                    valorTotal += item.valor_arrecadado;
+                }
+            });
+            const valorFormatado = valorTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            document.querySelector('.valor-total').innerHTML = valorFormatado;
+        });
+}
+
+function contarCampanhas() {
+    let valorTotal = 0
+
+    fetch("http://localhost:3300/campanha")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            data.forEach(item => {
+                if (item.organizador.id == idPerfil) {
+                    valorTotal += 1
+                    document.querySelector('.n-qtd-campanha').innerHTML = valorTotal
+                }
+            })
+        })
+}
+
+var itemCampanha = document.querySelector('.card')
+
+function carregarCampanhas() {
+    fetch("http://localhost:3300/campanha")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            data.forEach(item => {
+                if (item.organizador.id == idPerfil) {
+                    let novoItem = itemCampanha.cloneNode(true)
+                    novoItem.classList.remove('occult')
+
+                    let imgCampanha = novoItem.querySelector('.img-campanha')
+                    let nomeCampanha = novoItem.querySelector('.nome-campanha')
+                    let descCampanha = novoItem.querySelector('.descricao-campanha')
+
+                    nomeCampanha.innerHTML = item.titulo
+                    descCampanha.innerHTML = item.descricao
+                    imgCampanha.src = `../../../back/${item.imagens[0].caminho_imagem}`
+
+                    document.querySelector('.cards').appendChild(novoItem)
+                }
+            })
+        })
+}
