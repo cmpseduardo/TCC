46a01157db60c68246cdfd20fbac5cbd3252ea13
cmpseduardo <camposeduardov@gmail.com>
2023-05-25 08:15:53 -0300

    25-05-2023

--- frontend/pages/perfil/index.js
+++ frontend/pages/perfil/index.js
@@ -7,14 +7,17 @@ function carregarPerfil() {
         })
         .then((data) => {
             const item = data.find(item => item.id == idPerfil);
-
             let nomeOrganizador = document.querySelector('.nome-organizador-info')
             let tipoConta = document.querySelector('.tipo-conta')
+            let descPerfil = document.querySelector('.desc-perfil')
 
             nomeOrganizador.innerHTML = item.nome
             if (item.cnpj != null) {
-                tipoConta.innerHTML = "Jurídica"
+                tipoConta.innerHTML = "Pessoa Jurídica"
             }
-
+            else if (item.cpf != null) {
+                tipoConta.innerHTML = "Pessoa Física"
+            }
+            descPerfil.innerHTML = item.telefone //Trocar para descrição
         })
 }
