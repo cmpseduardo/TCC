8fc585439c3768d4088f98cb25be6c55cbcc5355
cmpseduardo <camposeduardov@gmail.com>
2023-06-01 11:30:44 -0300

    01-06-2023

--- back/src/controller/cadastro.js
+++ back/src/controller/cadastro.js
@@ -10,42 +10,44 @@ const prisma = new PrismaClient();
 
 const createAcesso = async (req, res) => { //OK
   let acesso = await prisma.nivelAcesso.create({
-      data: req.body
+    data: req.body
   });
   res.status(200).json(acesso).end();
 }
 
 const updateAcesso = async (req, res) => { //OK
   let acesso = await prisma.nivelAcesso.update({
-      where: {
-          id:Number (req.params.id)
-      },
-      data: req.body
+    where: {
+      id: Number(req.params.id)
+    },
+    data: req.body
   });
   res.status(200).json(acesso).end();
 }
 
 const create = async (req, res) => { //ESTÁ OK
-  bcrypt.genSalt(10, function(err, salt) {
-      if (err == null) {
-        bcrypt.hash(req.body.senha, salt, async function(errCrypto, hash) {
-          if(errCrypto == null){
-              req.body.senha = hash
-            
-              const cadastro = await prisma.cadastro.create({
-                  data: req.body
-        })
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err == null) {
+      bcrypt.hash(req.body.senha, salt, async function (errCrypto, hash) {
+        if (errCrypto == null) {
+          req.body.senha = hash
 
-            res.status(200).json(cadastro).end()
-          } else {
-            res.status(500).json(errCrypto).end()
-          }
-        });
+          const cadastro = await prisma.cadastro.create({
+            data: req.body
+          })
 
-          } else {
-        res.status(500).json(err).end()
-          }
-        })
+          res.status(200).json(cadastro).end()
+        } else {
+          console.log(errCrypto)
+          res.status(500).json(errCrypto).end()
+        }
+      });
+
+    } else {
+      console.log(err)
+      res.status(500).json(err).end()
+    }
+  })
 }
 
 const createImagem = async (req, res) => { //OK
@@ -95,49 +97,49 @@ const createImagem = async (req, res) => { //OK
     });
   });
 };
- 
+
 const read = async (req, res) => {
   let cadastro = await prisma.cadastro.findMany({
-      select: {
+    select: {
+      id: true,
+      nome: true,
+      descricao: true,
+      cpf: true,
+      cnpj: true,
+      email: true,
+      telefone: true,
+      senha: true,
+      instagram: true,
+      facebook: true,
+      twitter: true,
+      whatsapp: true,
+      site: true,
+      acesso: {
+        select: {
           id: true,
-          nome: true,
-          descricao: true,
-          cpf: true,
-          cnpj: true,
-          email: true,
-          telefone: true,
-          senha: true,
-          instagram: true,
-          facebook: true,
-          twitter: true,
-          whatsapp: true,
-          site: true,
-          acesso: {
-              select: {
-                  id: true,
-                  tipo: true
-              }
-          },
-          imagens: {
-            select: {
-              id: true,
-              caminho_imagem: true,
-            }
-          }
+          tipo: true
         }
-      });
-      res.status(200).json(cadastro).end();
+      },
+      imagens: {
+        select: {
+          id: true,
+          caminho_imagem: true,
+        }
+      }
+    }
+  });
+  res.status(200).json(cadastro).end();
 }
 
 const update = async (req, res) => {
-    let cadastro = await prisma.cadastro.update({
-        where: {
-            id:Number (req.params.id)
-        },
-        data: req.body
-    });
+  let cadastro = await prisma.cadastro.update({
+    where: {
+      id: Number(req.params.id)
+    },
+    data: req.body
+  });
 
-    res.status(200).json(cadastro).end();
+  res.status(200).json(cadastro).end();
 }
 
 const remove = async (req, res) => {
@@ -151,43 +153,43 @@ const remove = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    const cadastro = await prisma.cadastro.findFirstOrThrow({
-      where: {
-        email: req.body.email
-      }
-    }).then((value) => { return (value) })
-      .catch((err) => { return { "erro": "Usuário não encontrado", "validation": false } })
-  
-    if (cadastro.erro == null) {
-      bcrypt.compare(req.body.senha, cadastro.senha).then((value) => {
-        if (value) {
-          let data = { "uid": cadastro.id, "role": cadastro.acesso }
-          jwt.sign(data, process.env.KEY, { expiresIn: '1m' }, function (err2, token) {
-            console.log(err2)
-            if (err2 == null) {
-              res.status(200).json({ "token": token, "uid": cadastro.id, "uname": cadastro.nome, "validation": true }).end()
-            } else {
-              res.status(500).json(err2).end()
-            }
-  
-          })
-        } else {
-          res.status(201).json({ "erro": "Senha inválida", "validation": false }).end()
-        }
-      })
-    } else {
-      res.status(404).json(cadastro ).end()
+  const cadastro = await prisma.cadastro.findFirstOrThrow({
+    where: {
+      email: req.body.email
     }
+  }).then((value) => { return (value) })
+    .catch((err) => { return { "erro": "Usuário não encontrado", "validation": false } })
+
+  if (cadastro.erro == null) {
+    bcrypt.compare(req.body.senha, cadastro.senha).then((value) => {
+      if (value) {
+        let data = { "uid": cadastro.id, "role": cadastro.acesso }
+        jwt.sign(data, process.env.KEY, { expiresIn: '1m' }, function (err2, token) {
+          console.log(err2)
+          if (err2 == null) {
+            res.status(200).json({ "token": token, "uid": cadastro.id, "uname": cadastro.nome, "validation": true }).end()
+          } else {
+            res.status(500).json(err2).end()
+          }
+
+        })
+      } else {
+        res.status(201).json({ "erro": "Senha inválida", "validation": false }).end()
+      }
+    })
+  } else {
+    res.status(404).json(cadastro).end()
   }
+}
 
 module.exports = {
-    create,
-    read,
-    update,
-    remove,
-    login,
-    createAcesso,
-    updateAcesso,
-    createImagem
-    
+  create,
+  read,
+  update,
+  remove,
+  login,
+  createAcesso,
+  updateAcesso,
+  createImagem
+
 }
--- frontend/pages/register/index.js
+++ frontend/pages/register/index.js
@@ -24,12 +24,69 @@ function cadastrarPessoaFisica() {
     }
 }
 
-function cadastrarPessoaJuridica(){
-    const dados = {
-        "nome": document.querySelector("#nome-pessoa-fisica"),
-        "cpf": document.querySelector("#cpf"),
-        "email": document.querySelector("#email-pessoa-fisica"),
-        "telefone": `${document.querySelector(".ddd-pessoa-fisica")}` + `${document.querySelector("#numero-celular-pessoa-fisica")}`,
-        "senha": document.querySelector("#senha-2-pessoa-fisica"),
-    } 
+function cadastrarPessoaJuridica() {
+    if (document.querySelector("#senha-2-pessoa-juridica").value == document.querySelector("#senha-1-pessoa-juridica").value) {
+        const acessoJSON = {
+            "tipo": 2
+        };
+
+        let loginJSON = {
+            "nome": document.querySelector("#nome-instituicao").value,
+            "descricao": null,
+            "cpf": null,
+            "cnpj": document.querySelector("#cnpj").value,
+            "email": document.querySelector("#email-pessoa-juridica").value,
+            "telefone": `${document.querySelector("#ddd-pessoa-juridica").value}` + `${document.querySelector("#numero-celular-pessoa-juridica").value}`,
+            "senha": document.querySelector("#numero-celular-pessoa-juridica").value,
+            "instagram": null,
+            "facebook": null,
+            "twitter": null,
+            "whatsapp": null,
+            "site": null,
+            "acessoid": null
+
+        };
+
+        fetch('http://localhost:3300/cadastroAcesso', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(acessoJSON)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Resposta do primeiro JSON:', data);
+                loginJSON.acessoid = data.id;
+
+                fetch('http://localhost:3300/cadastro', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(loginJSON)
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log('Resposta do segundo JSON:', data);
+                        alert("Estouro")
+                    })
+                    .catch(error => {
+                        console.error('Erro na requisição do segundo JSON:', error);
+                    });
+            })
+            .catch(error => {
+                console.error('Erro na requisição do primeiro JSON:', error);
+            });
+
+    } else {
+        alert('puts')
+    }
+
 }
+
+
+
+
+
+
